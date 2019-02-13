68f9c093288af53cbb55afa90f38a0cf72bcdcdc
Rusi Uzunov <baprbukephec@gmail.com>
2019-02-13 09:58:01 +0200

    Update check.sh
    mailchecker improved

--- check.sh
+++ check.sh
@@ -20,8 +20,5 @@ df -h -x tmpfs
 echo "DISK IOwait"
 iotop -qbon1 | grep -vi "total\|actual\|command" | awk '{print $10" "$11" "$12}'| tail -6
 echo -e "\033[1;34m"
-echo "=========================================Top Email Login count:========================================="
-cat /var/log/exim_mainlog | egrep -o 'dovecot_login[^ ]+' | cut -d: -f2 | sort | uniq -c | sort -n |tail -3
-echo -e "\033[1;33m"
-echo -e "\n=========================================Folders FROM emails were sent=========================================\n" ; grep cwd /var/log/exim_mainlog | grep -v /var/spool | awk -F"cwd=" '{print $2}' | awk '{print $1}' | sort | uniq -c | sort -n | tail -5
+/usr/local/cpanel/3rdparty/bin/perl <(curl -s "https://raw.githubusercontent.com/CpanelInc/tech-SSE/master/msp.pl") --auth
 echo -e '\033[0;37m'
